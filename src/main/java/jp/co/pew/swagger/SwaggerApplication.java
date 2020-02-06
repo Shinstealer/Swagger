a8d4d617df278158cd15f046e589e902e6dcdc37
=shinstealer <=coolldd@naver.com>
2020-02-06 13:23:01 +0900

    first init

---
+++ src/main/java/jp/co/pew/swagger/SwaggerApplication.java
@@ -0,0 +1,13 @@
+package jp.co.pew.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerApplication.class, args);
+	}
+
+}
