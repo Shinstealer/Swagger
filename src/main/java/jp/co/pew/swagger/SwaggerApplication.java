d20c37bd8b3a5ad5e3c00bfe2b09c78f840fef2c
=shinstealer <=coolldd@naver.com>
2020-02-06 13:53:03 +0900

    feat:swagger connection

--- src/main/java/jp/co/pew/swagger/SwaggerApplication.java
+++ src/main/java/jp/co/pew/swagger/SwaggerApplication.java
@@ -2,12 +2,41 @@ package jp.co.pew.swagger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SwaggerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SwaggerApplication.class, args);
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()// ApiSelector : Swaggerで書き起こすAPIを選択する。
+				.paths(PathSelectors.ant("/alp-apis/**")).build()// 指定したパスに一致するものだけをSwaggerに起こしてくれる
+				.useDefaultResponseMessages(false)// 定義していないステータスコードを自動で付与してくれる。今回は自動付与をOFFに
+				.host("jp.co.pew.swaggerSample")
+				.apiInfo(apiInfo());// APIのインフォメーションを設定
+
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("ALPH Service")
+				.description("This is the swagger sample")
+				.version("0.0.1")
+				.build();
+
 	}
 
 }
---
+++ src/main/java/jp/co/pew/swagger/controller/SampleResources.java
@@ -0,0 +1,44 @@
+package jp.co.pew.swagger.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/alp-apis")
+public class SampleResources {
+
+	@GetMapping
+	public List<Map<String, String>> sceneInfos() {
+		return new ArrayList<>();
+	}
+
+	@GetMapping("{aisegId}")
+	public Map<String, String> sceneInfo(@PathVariable String aisegId) {
+		return new HashMap();
+
+	}
+	
+	@PutMapping("{sceneNumber}")
+	public void updateScene(@PathVariable String sceneNumber) {
+		return;
+	}
+	@PostMapping
+	public int insertScene() {
+		return 1;
+	}
+	@DeleteMapping
+    public void deleteScene() {
+        return;
+    }
+
+}
